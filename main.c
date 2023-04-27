0ad5da99ae4e6e3bf2841a335de7e5952c788f01
clumsykun <clumsykun@qq.com>
2023-04-27 11:27:39 +0800

    makefile

--- main.c
+++ main.c
@@ -5,6 +5,7 @@ int debug() {
 
     double data[] = {1,2,3,4,333,6, 112};
     double out = stats_std(data, 7, 1);
+    printf("%f\n", out);
 
     return 0;
 }
---
+++ makefile
@@ -0,0 +1,2 @@
+dev:
+	/usr/bin/gcc -fdiagnostics-color=always -g -lm -std=c99 /home/dev/zeno/main.c -o /home/dev/zeno/main
