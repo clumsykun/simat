351655524bbca4f3b783ff0e09af7b32b3f14a6c
clumsykun <clumsykun@qq.com>
2023-04-27 09:15:14 +0800

    swap charp

--- include/dev.c
+++ include/dev.c
@@ -11,12 +11,19 @@ int swap_intp(int *a, int* b) {
     return 0;
 }
 
-
+/*
+ * int:
+ * a -> *a -> char1
+ * b -> *b -> char2
+ * out:
+ * a -> *a -> char2
+ * b -> *b -> char1
+*/
 int swap_charp(char **a, char **b) {
 
-    char *tmp = *a;
-    *a = *b;
-    *b = tmp;
+    char *tmp = *a;  // tmp = *a -> char1
+    *a = *b;  // *a = *b -> char2, **a -> char2
+    *b = tmp;  // *b = tmp -> char1
 
     return 0;
 }
