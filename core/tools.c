511ed523ac7418c213438cce0e2571b643616f69
clumsykun <clumsykun@qq.com>
2023-05-03 10:29:10 +0800

    rename

--- core/tools.c
+++ core/tools.c
@@ -3,35 +3,49 @@
 #include "tools.h"
 
 /* the standard way to free memory */
-void _std_free(void *ptr)
+void __std_free(void *ptr)
 {
     free(ptr);
     ptr = NULL;
 }
 
-void _swap_int(int *left, int *right)
+void __swap_int(int *left, int *right)
 {
     int tmp = *left;
     *left = *right;
     *right = tmp;
 }
 
-void _swap_double(double *left, double *right)
+void __swap_double(double *left, double *right)
 {
     double tmp = *left;
     *left = *right;
     *right = tmp;
 }
 
-void _swap_double_p(double **left, double **right)
+void __swap_double_p(double **left, double **right)
 {
     double tmp = **left;
     **left = **right;
     **right = tmp;
 }
 
-void dtype_error(void)
+size_t __size_t_dtype_unknown_error(void)
 {
-    fprintf(stderr, "Data type error! Exiting...\n");
+    fprintf(stderr, "Data type unknown error! Exiting...\n");
+    exit(EXIT_FAILURE);
+    return 0;
+}
+
+double __double_dtype_unknown_error(void)
+{
+    fprintf(stderr, "Data type unknown error! Exiting...\n");
+    exit(EXIT_FAILURE);
+    return 0;
+}
+
+void __dtype_unknown_error(void)
+{
+    fprintf(stderr, "Data type unknown error! Exiting...\n");
     exit(EXIT_FAILURE);
 }
--- core/tools.h
+++ core/tools.h
@@ -1,10 +1,12 @@
 #ifndef CORE_TOOLS_H
 #define CORE_TOOLS_H
 
-void _std_free(void *ptr);
-void _swap_int(int *left, int *right);
-void _swap_double(double *left, double *right);
-void _swap_double_p(double **left, double **right);
-void dtype_error(void);
+void __std_free(void *ptr);
+void __swap_int(int *left, int *right);
+void __swap_double(double *left, double *right);
+void __swap_double_p(double **left, double **right);
+size_t __size_t_dtype_unknown_error(void);
+double __double_dtype_unknown_error(void);
+void __dtype_unknown_error(void);
 
 #endif /* CORE_TOOLS_H */
