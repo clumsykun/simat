76f6919372cb95ab9f8ab49216ea706bd8d718f7
clumsykun <clumsykun@qq.com>
2023-05-07 17:48:15 +0800

    __st_out_range_error

--- core/simat/flags.c
+++ core/simat/flags.c
@@ -1,12 +1,13 @@
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <assert.h>
 #include "flags.h"
-
 
 bool __st_is_error = false;
 bool __st_access_error = false;
 bool __st_dtype_error = false;
+bool __st_out_range_error = false;
 
 double __st_raise_access_error(void)
 {
@@ -22,13 +23,31 @@ size_t __st_raise_dtype_error(void)
     return 0;
 }
 
+double __st_raise_out_range_error(void)
+{
+    __st_is_error = true;
+    __st_out_range_error = true;
+    return 0;
+}
+
 /**
  * check program is correct or not after any operation
  * probably need some advanced handling
  */
 void __st_check()
 {
+
+    if (__st_access_error)
+        printf("Error: Access failed!\n");
+
+    if (__st_dtype_error)
+        printf("Error: Data type not found!\n");
+
+    if (__st_out_range_error)
+        printf("Error: Index out of range!\n");
+
     assert(!__st_is_error);
     assert(!__st_access_error);
     assert(!__st_dtype_error);
+    assert(!__st_out_range_error);
 }
