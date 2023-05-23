01cc3cc6657683a7c9fb8e92a4d055fcfdaf78cf
clumsykun <clumsykun@qq.com>
2023-05-23 14:21:30 +0800

    cos similarity of 2 view

--- core/simat/distance.c
+++ core/simat/distance.c
@@ -5,5 +5,11 @@
  */
 double st_dist_cos_vv(st_vector *v1, st_vector *v2)
 {
-    return st_vec_dot(v1,v2)/(st_vec_norm(v1)*st_vec_norm(v2));;
+    return st_vec_dot(v1,v2)/(st_vec_norm(v1)*st_vec_norm(v2));
+}
+
+
+double st_dist_cos_ww(st_view *w1, st_view *w2)
+{
+    return st_view_dot(w1,w2)/(st_view_norm(w1)*st_view_norm(w2));
 }
