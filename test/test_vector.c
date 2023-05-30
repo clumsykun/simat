e2c52705bca1d2816d9be26093c4558488235b96
clumsykun <clumsykun@qq.com>
2023-05-30 14:50:55 +0800

    use memcpy to initialize

--- test/test_vector.c
+++ test/test_vector.c
@@ -5,13 +5,16 @@
  */
 st_vector *test_vec_1(void)
 {
-
     st_vector *vec = st_new_vector(5);
-    st_vec_assign(vec, 0, 3.141592653589793);
-    st_vec_assign(vec, 1, 2.718281828459045);
-    st_vec_assign(vec, 2, 0.577215664901532);
-    st_vec_assign(vec, 3, 1.414213562373095);
-    st_vec_assign(vec, 4, 1.618033988749894);
+    double arr[5] = {
+        3.141592653589793,
+        2.718281828459045,
+        0.577215664901532,
+        1.414213562373095,
+        1.618033988749894
+    };
+
+    memcpy(vec->data->head, arr, sizeof(arr));
     return vec;
 }
 
@@ -20,13 +23,16 @@ st_vector *test_vec_1(void)
  */
 st_vector *test_vec_2(void)
 {
-
     st_vector *vec = st_new_vector(5);
-    st_vec_assign(vec, 0, 6.67428);
-    st_vec_assign(vec, 1, 9.10938215);
-    st_vec_assign(vec, 2, 6.62606896);
-    st_vec_assign(vec, 3, 5.2917720859);
-    st_vec_assign(vec, 4, 1.602176487);
+    double arr[5] = {
+        6.67428,
+        9.10938215,
+        6.62606896,
+        5.2917720859,
+        1.602176487
+    };
+
+    memcpy(vec->data->head, arr, sizeof(arr));
     return vec;
 }
 
