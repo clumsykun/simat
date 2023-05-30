ca3d7d19975ff564c5a16b1d00fb9144ecd4d17b
clumsykun <clumsykun@qq.com>
2023-05-30 16:11:18 +0800

    test matrix

--- test/test_matrix.c
+++ test/test_matrix.c
@@ -16,12 +16,36 @@ result *test__st_mat_min(result *rp)
     return rp;
 }
 
+result *test__st_mat_max(result *rp)
+{
+    rp->name = "st_mat_max";
+    double target = 7.9;
+
+    st_dataset *iris = st_load_iris();
+    rp->value = !equal(st_mat_max(iris->X), target);
+
+    return rp;
+}
+
+result *test__st_mat_scale(result *rp)
+{
+    rp->name = "st_mat_scale";
+
+    st_dataset *iris = st_load_iris();
+    st_mat_scale(iris->X, 0, 1);
+    rp->value = !equal(st_mat_min(iris->X), 0);
+    rp->value = !equal(st_mat_max(iris->X), 1);
+
+    return rp;
+}
 
 int test__matrix(void)
 {
     printf("unit test of matrix start:\n");
 
     call_test(test__st_mat_min);
+    call_test(test__st_mat_max);
+    call_test(test__st_mat_scale);
 
     printf("\n");
     return 0;
