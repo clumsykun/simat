80ded4e757d43fa2bb9c897b1571eb02dad1d346
clumsykun <clumsykun@qq.com>
2023-06-07 23:45:55 +0800

    arrange head file

--- core/simat/basic/st_blas.h
+++ core/simat/basic/st_blas.h
@@ -9,8 +9,6 @@
  * =================================================================================================
  */
 
-CBLAS_INDEX cblas_idmin(OPENBLAS_CONST BLASINT n, OPENBLAS_CONST double *x, OPENBLAS_CONST BLASINT incx);
-CBLAS_INDEX cblas_idmax(OPENBLAS_CONST BLASINT n, OPENBLAS_CONST double *x, OPENBLAS_CONST BLASINT incx);
 double cblas_dnrm2(OPENBLAS_CONST BLASINT N, OPENBLAS_CONST double *X, OPENBLAS_CONST BLASINT incX);
 void cblas_dcopy(OPENBLAS_CONST BLASINT n, OPENBLAS_CONST double *x, OPENBLAS_CONST BLASINT incx, double *y, OPENBLAS_CONST BLASINT incy);
 void cblas_daxpy(OPENBLAS_CONST BLASINT n, OPENBLAS_CONST double alpha, OPENBLAS_CONST double *x, OPENBLAS_CONST BLASINT incx, double *y, OPENBLAS_CONST BLASINT incy);
@@ -20,4 +18,6 @@ void cblas_daxpy(OPENBLAS_CONST BLASINT n, OPENBLAS_CONST double alpha, OPENBLAS
  * =================================================================================================
  */
 
+CBLAS_INDEX cblas_idmax(OPENBLAS_CONST BLASINT n, OPENBLAS_CONST double *x, OPENBLAS_CONST BLASINT incx);
+CBLAS_INDEX cblas_idmin(OPENBLAS_CONST BLASINT n, OPENBLAS_CONST double *x, OPENBLAS_CONST BLASINT incx);
 double cblas_dsum(OPENBLAS_CONST BLASINT n, OPENBLAS_CONST double *x, OPENBLAS_CONST BLASINT incx);
