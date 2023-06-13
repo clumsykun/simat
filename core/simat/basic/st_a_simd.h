06f1844e5f66fdced22f59ba7edad1d20b98a8c8
clumsykun <clumsykun@qq.com>
2023-06-13 09:38:08 +0800

    use simd to cal euclid

--- core/simat/basic/st_a_simd.h
+++ core/simat/basic/st_a_simd.h
@@ -1,21 +1,34 @@
 
+#define st_sse4_1
+
 /* =================================================================================================
- * SSE2 settings
+ * SSE4.1 settings
  */
+
+#ifdef st_sse4_1
 
 #define st_mi     __m128i
 #define st_md     __m128d
 
-#define st_load_d64(p)                _mm_loadu_pd((double *)p)
-#define st_load_i32(p)                _mm_loadu_si128((__m128i *)p)
-#define st_store_d64(p, d128)         _mm_storeu_pd((double *)p, d128)
-#define st_store_i32(p, i128)         _mm_storeu_si128((__m128i *)p, i128)
+#define st_load_d64(p)                _mm_load_pd((double *)p)
+#define st_load_i32(p)                _mm_load_si128((__m128i *)p)
+#define st_store_d64(p, d128)         _mm_store_pd((double *)p, d128)
+#define st_store_i32(p, i128)         _mm_store_si128((__m128i *)p, i128)
 #define st_m_mul_d64(d128_a, d128_b)  _mm_mul_pd(d128_a, d128_b)
 #define st_m_add_i32(i128_a, i128_b)  _mm_add_epi32(i128_a, i128_b)
+#define st_m_add_d64(d128_a, d128_b)  _mm_add_pd(d128_a, d128_b)
+
+#define st_m_sub_i32(i128_a, i128_b)  _mm_sub_epi32(i128_a, i128_b)
+
+#define st_m_sub_d64(d128_a, d128_b)  _mm_sub_pd(d128_a, d128_b)
 #define st_m_xor_i128(i128_a, i128_b) _mm_xor_si128(i128_a, i128_b)
 #define st_m_or_i128(i128_a, i128_b)  _mm_or_si128(i128_a, i128_b)
+
+#define st_m_zero_d64() _mm_setzero_pd()
 
 #define st_m_size_d64 2
 #define st_m_size_i32 4
 #define st_m_size_u8  16
 #define st_m_size_b   16
+
+#endif /* st_sse4_1 */
--- core/simat/st_distance.c
+++ core/simat/st_distance.c
@@ -1,21 +1,65 @@
 #include <math.h>
+#include <immintrin.h>
 #include "st_distance.h"
+
+static double
+__dist_euclid_double(size_t n, double *a, double *b)
+{
+    size_t bsize     = st_m_size_d64;
+    size_t batches   = n / bsize;
+    size_t remainder = n % bsize;
+
+    st_md *pa = (st_md *) a;
+    st_md *pb = (st_md *) b;
+
+    st_md pk_sum_diff = st_m_zero_d64();
+    st_d64 arr_sum_diff[st_m_size_d64];
+    st_d64 diff, sum_diff = 0;
+
+    while (batches--) {
+
+        st_md pk_a = st_load_d64((st_d64 *)pa++);
+        st_md pk_b = st_load_d64((st_d64 *)pb++);
+        st_md pk_diff = st_m_sub_d64(pk_a, pk_b);
+        pk_diff = st_m_mul_d64(pk_diff, pk_diff);
+        pk_sum_diff = st_m_add_d64(pk_sum_diff, pk_diff);
+    }
+
+    st_store_d64(arr_sum_diff, pk_sum_diff);
+
+    for (size_t i = 0; i < st_m_size_d64; i++)
+        sum_diff += arr_sum_diff[i];
+
+    a = (st_d64 *)pa;
+    b = (st_d64 *)pb;
+
+    while (remainder--) {
+        diff = (*a++) - (*b++);
+        sum_diff += diff * diff;
+    }
+
+    return sqrt(sum_diff);
+}
+
 
 st_decimal
 st_dist_euclid(st_vector *a, st_vector *b)
 {
     if (st_check_vec_len(a, b->len))
         __st_raise_length_error();
-    
-    st_d64 diff, sum_square = 0;
-    size_t i;
-    void *pa, *pb;
-    for __st_iter_vector2(i, pa, pb, a, b) {
-            diff = __st_access_p(pa, a->dtype) - __st_access_p(pb, b->dtype);
-            sum_square += diff * diff;
-    }
 
-    return sqrt(sum_square);
+    if (st_check_vec_dtype(a, b->dtype))
+        __st_raise_dtype_error();
+
+    size_t n = a->len;
+
+    switch (a->dtype) {
+        case st_dtype_d64:
+            return __dist_euclid_double(n, a->data->head, b->data->head);
+        
+        default:
+            return 0;
+    }
 }
 
 /* cosine similarity of vector v1 and v2. */
